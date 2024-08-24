e4aac4c4a62989d53b452525a61e78e82f6713d3
Mayank <9084735+mayank99@users.noreply.github.com>
2024-08-24 18:29:56 -0400

    make it so!

---
+++ README.md
@@ -0,0 +1,15 @@
+# 11ty-jsx-deno
+
+Minimal example showing how to use JSX/TSX templates in 11ty using Deno and Preact.
+
+Dev server:
+
+```sh
+deno run dev
+```
+
+Build:
+
+```sh
+deno run build
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+	return (
+		<html lang="en">
+			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width" />
+				<title>Document</title>
+			</head>
+			<body>
+				<h1>Hello!</h1>
+			</body>
+		</html>
+	);
+}
