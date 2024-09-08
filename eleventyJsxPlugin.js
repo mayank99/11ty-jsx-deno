421c20be51e078a0d8dd3363d3f1cd9f39910c92
Mayank <9084735+mayank99@users.noreply.github.com>
2024-09-08 16:21:24 -0400

    add preprocessor

--- eleventyJsxPlugin.js
+++ eleventyJsxPlugin.js
@@ -4,6 +4,13 @@ import { isValidElement } from "preact";
 /** @param {import("npm:11ty.ts").EleventyConfig} eleventyConfig */
 export function eleventyJsxPlugin(eleventyConfig) {
 	eleventyConfig.addTemplateFormats("tsx,jsx");
+	eleventyConfig.addPreprocessor(
+		"jsx-transform",
+		["jsx", "tsx"],
+		(data, content) => {
+			console.log({ data, content }); // ‼️ content is empty
+		},
+	);
 	eleventyConfig.addExtension(["tsx", "jsx"], {
 		key: "11ty.js",
 		compile: function () {
